810985f24d790a1bfc1b2dd1ef163166a92887a1
layan3th <layan.3othpro@gmail.com>
2026-09-15 23:34:05 +0300

    Remove unnecessary print statements

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,9 +4,6 @@ public class HelloWorld {
     public static void main(String[]args){
     System.out.println("Hello From feature branch ");
     System.out.println("Frist name:layan");
-    System.out.println("last name:Altamimi");
-    System.out.println("123");
-
-    
+    System.out.println("last name:Altamimi");    
 } 
 }
